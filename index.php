b84eb79631f201fc9fd59e01d2bfe26cbb88782c
kimia <kimia.ehsani.dolati.al@iespoligonosur.org>
2021-11-02 22:25:33 +0100

    db

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <h3>Sociedad Protectora de Animales de ALLLIFE</h3>
     Trabajamos desde el año 1971 con el objetivo de acoger, amparar y promover la adopción de los animales abandonados. Gestionamos centros de acogida con una filosofía proteccionista, luchamos por la vida de los animales y trabajamos día tras día para evitar su sufrimiento. Nuestra tarea también es divulgativa: concienciamos a la sociedad sobre la tenencia responsable de los animales de compañía, sobre los derechos de los animales, sobre los beneficios de la adopción y sobre una buena convivencia entre los animales y los ciudadanos.<br>
     Acerca de la base de datos, tenemos campos id, nombre ,dni, edad , sexo, fecha de alta, y foto del gato.
-    Puedes dar de alta o de baja a un gato, actualizar los datos de un gato, obtener los datos de todos los gatos registrados o solo de uno en específico.
+    Puedes dar de alta o de baja a un gato, actualizar los datos de un gato, obtener los datos de todos los gatos registrados o solo de uno en específico. 
 
     <h4>Actividades principales</h4>
     <ul>
